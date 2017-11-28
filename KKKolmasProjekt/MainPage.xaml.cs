472252f3bf87e3fcb52202756b3c282c8f068fbb
Dan-Andre Leesment <dan-andre.leesment@vikk.ee>
2017-11-28 14:20:04 +0200

    Esimene Commit

---
+++ KKKolmasProjekt/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace KKKolmasProjekt
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            string Kasutajanimi = textbox1.Text;
+            string Parool = passwordbox1.Password;
+
+                if ((textbox1.Text == "Admin") && (passwordbox1.Password == "Passw0rd"))
+                {
+                textbox5.Text = "Sisse logitud";
+            }          
+                else
+            {
+                textbox5.Text = "Vale kasutajanimi või parool";
+            }
+        }
+    }
+}
